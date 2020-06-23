7e16e5897dd28c76ed8619512982a117b2521a0e
steja2891 <67269451+steja2891@users.noreply.github.com>
2020-06-23 12:37:57 +0530

    i added cube opeartion

--- area_of_square.py
+++ area_of_square.py
@@ -2,3 +2,8 @@ s = int(input("Enter side value"))
 
 a = s*s
 print(a)
+
+print("****************************************")
+volume  = s*s*s
+print(volume)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
